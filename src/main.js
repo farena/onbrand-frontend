012fc1ead289b9d1646651f72c00ed5382949812
Farena <fedarena@gmail.com>
2020-07-24 18:40:59 +0100

    Implement Vue Noty

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import VueNoty from 'vuejs-noty';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import errorHandler from './mixins/errorHandler';
 
 Vue.config.productionTip = false;
+
+/** Plugins */
+Vue.use(VueNoty);
 
 /** General Mixin */
 Vue.mixin(errorHandler);
