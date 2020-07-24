bcb3890e6c1e4f36960db30102024fc1035dceb5
Farena <fedarena@gmail.com>
2020-07-24 19:45:26 +0100

    Installing and Implementing Farena/VueTables

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import VueNoty from 'vuejs-noty';
+import VueTables from 'vue-tables';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import errorHandler from './mixins/errorHandler';
+import 'vue-tables/dist/vue-tables.css';
+import './assets/sass/vue-tables-custom.scss';
 import 'vuejs-noty/dist/vuejs-noty.css';
 
 Vue.config.productionTip = false;
 
 /** Plugins */
 Vue.use(VueNoty);
+Vue.use(VueTables);
 
 /** General Mixin */
 Vue.mixin(errorHandler);
