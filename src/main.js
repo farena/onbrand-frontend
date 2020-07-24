fec008a2b1ff3e09bbed9f62b0b54d3deb1511aa
Farena <fedarena@gmail.com>
2020-07-24 19:44:54 +0100

    Vue Noty Css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import errorHandler from './mixins/errorHandler';
+import 'vuejs-noty/dist/vuejs-noty.css';
 
 Vue.config.productionTip = false;
 
